41699aa1bbd08c53f9f4609c8a762d9ec44cfabb
diegosotosoto <60042019+diegosotosoto@users.noreply.github.com>
2023-04-27 00:22:25 -0400

    Removed Trash

--- roanfood.cl/cierra_sesion.php
+++
@@ -1,5 +0,0 @@
-<?php
-setcookie("hkjh41laa8u4l1k23jhlkj1387s76d8as76a9sd8",$galletita_mail, time()-1);
-setcookie("hkjh41laa8u4l1k23jhl54fgd897d1k23jhlkj14",$galletita_user, time()-1);
-header('Location: login.php');
-?>
