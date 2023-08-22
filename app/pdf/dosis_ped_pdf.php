b394f3d41fd6eb3cd7536fa1c1fa3815d4aff315
diegosotosoto <60042019+diegosotosoto@users.noreply.github.com>
2023-08-22 05:03:53 -0400

    bugfixes

--- app/pdf/dosis_ped_pdf.php
+++ app/pdf/dosis_ped_pdf.php
@@ -1,32 +1,42 @@
 <?php
-// Recibir las variables enviadas por POST
-$peso_pdf = $_POST['peso'];
-$talla_pdf = $_POST['talla'];
-$atropina_pdf = $_POST['atropina'];
-// ... Repite esto para todas las otras variables ...
 
-// Cargar la librería FPDF
-    require('tfpdf.php');//
+require('../pdf/tfpdf.php');//
 
-// Crear una nueva instancia de FPDF
-    $pdf = new tFPDF('P','mm','Letter');
-    $pdf->AddPage();
-    $pdf->SetCreator('www.anestesiauach.cl');
-    $pdf->SetMargins(20,25,10);
-    $pdf->AddFont('DejaVu','','DejaVuSansCondensed.ttf',true);
-    $pdf->SetFont('DejaVu','',12);
-
-// Agregar contenido al PDF
-$pdf->Cell(0, 10, 'Reporte de Dosificación', 0, 1, 'C');
-
+// Crear nuevo documento PDF
+$pdf = new tFPDF('P','mm','Letter');
+$pdf->AddPage();
 $pdf->SetFont('Arial', '', 12);
-$pdf->Cell(0, 10, 'Peso: ' . $peso_pdf . ' kg', 0, 1);
-$pdf->Cell(0, 10, 'Talla: ' . $talla_pdf . ' cm', 0, 1);
-$pdf->Cell(0, 10, 'Dosis de Atropina: ' . $atropina_pdf . ' mg', 0, 1);
-// ... Repite esto para todas las otras variables ...
 
-// Guardar el PDF en un archivo o mostrarlo en pantalla
-    $pdf->Output('I', 'reporte.pdf');
+// Título de la tabla
+$pdf->SetFont('Arial', 'B', 12);
+$pdf->Cell(190, 10, utf8_decode('HISTORIA CLINICA PREANESTESICA'), 1, 1, 'C');
 
+// Primera fila: Identificación del paciente
+$pdf->SetFont('Arial', 'B', 12);
+$pdf->Cell(95, 10, utf8_decode('IDENTIFICACIÓN DEL PACIENTE'), 1, 0, 'C');
+$pdf->Cell(95, 10, utf8_decode('DATOS DEL PROCEDIMIENTO'), 1, 1, 'C');
 
+// Detalles de identificación del paciente
+$pdf->SetFont('Arial', '', 12);
+$pdf->Cell(47.5, 10, utf8_decode('Nombre'), 1, 0);
+$pdf->Cell(47.5, 10, '', 1, 0); // Espacio para escribir nombre del paciente
+$pdf->Cell(47.5, 10, utf8_decode('Fecha'), 1, 0);
+$pdf->Cell(47.5, 10, '', 1, 1); // Espacio para escribir fecha
+
+$pdf->Cell(47.5, 10, utf8_decode('Edad'), 1, 0);
+$pdf->Cell(47.5, 10, '', 1, 0); // Espacio para escribir edad
+$pdf->Cell(47.5, 10, utf8_decode('Hora'), 1, 0);
+$pdf->Cell(47.5, 10, '', 1, 1); // Espacio para escribir hora
+
+$pdf->Cell(47.5, 10, utf8_decode('Sexo'), 1, 0);
+$pdf->Cell(47.5, 10, '', 1, 0); // Espacio para escribir sexo
+$pdf->Cell(47.5, 10, utf8_decode('Servicio'), 1, 0);
+$pdf->Cell(47.5, 10, '', 1, 1); // Espacio para escribir servicio
+
+$pdf->Cell(47.5, 10, utf8_decode('Procedencia'), 1, 0);
+$pdf->Cell(47.5, 10, '', 1, 0); // Espacio para escribir procedencia
+$pdf->Cell(95, 10, '', 1, 1); // Espacio para escribir otros datos si es necesario
+
+// Guardar el PDF
+$pdf->Output();
 ?>
